835d075214611382758d9109d0550b4cb1328265
Rayane Ventura <rayyventura@gmail.com>
2022-05-09 02:10:19 +0200

    add:unit tests

--- tests/factories/recommendationsFactory.ts
+++ tests/factories/recommendationsFactory.ts
@@ -6,3 +6,9 @@ export const recommendation: CreateRecommendationData = {
   name: "Ghost",
   youtubeLink: "https://www.youtube.com/watch?v=Fp8msa5uYsc",
 };
+export const recommendationUnit: Recommendation = {
+  id: 1,
+  name: "Ghost",
+  youtubeLink: "https://www.youtube.com/watch?v=Fp8msa5uYsc",
+  score: 0,
+};
