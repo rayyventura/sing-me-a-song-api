d86c2fbb95820e672212a2fa177650e8e24e2a95
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-06-29 14:46:09 -0300

    Preparando revisão de código

--- src/routers/e2eTestRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { resetDatabase } from "../controllers/e2eTestController.js";
-
-const e2eTestRouter = Router();
-e2eTestRouter.post("/reset", resetDatabase);
-export default e2eTestRouter;
--- src/services/e2eTestService.ts
+++
@@ -1,4 +0,0 @@
-import { prisma } from "../database.js";
-export async function resetDatabase() {
-  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
-}
